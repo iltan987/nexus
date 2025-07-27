60379c026f1066c6d0ccad9befbac921f7865d82
iltan987 <iltancanerr@gmail.com>
2025-07-27 14:49:20 +0000

    Move eslint config into it's own module in eslint-config-package

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,34 +1,3 @@
-// @ts-check
-import eslint from '@eslint/js';
-import config from '@repo/eslint-config/eslint-prettier-config';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import nest from '@repo/eslint-config/nest';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...config,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-    },
-  },
-);
+export default nest;
