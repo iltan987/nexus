08b194c4663b8e0ef2485d0e5b49dfee60b6f341
iltan987 <iltancanerr@gmail.com>
2025-07-30 11:40:51 -0400

    Remove unnecessary UsersController from UsersModule

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
   providers: [PrismaService, UsersService],
 })
 export class UsersModule {}
