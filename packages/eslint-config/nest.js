219e6b7a23df815f11c0456d3d3df080ab7629ad
iltan987 <iltancanerr@gmail.com>
2025-07-30 10:07:30 -0400

    Add ESLint rule to enforce unused variable checks with specific ignore patterns

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -27,6 +27,17 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
   },
   eslintPluginPrettierRecommended
