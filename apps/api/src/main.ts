0da9c1616b4f7f8637c2aee451700bc641a6161b
iltan987 <iltancanerr@gmail.com>
2025-07-30 11:43:36 -0400

    Add custom logger configuration to NestFactory bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'Nexus',
+    }),
+  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
